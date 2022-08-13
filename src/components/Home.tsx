482dd5235dcb5f56c7d709b8ccecaada143710d5
davidf1000 <davidfauzi29@gmail.com>
2022-08-13 11:40:55 +0700

    add checkbox login

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -16,13 +16,43 @@ const backStyle = {
 
 
 
-const Home = (): JSX.Element => {
-    const [loading, setLoading] = useState<boolean>(false)
+interface Check {
+    transporter: boolean;
+    shipper: boolean;
+}
 
+const Home = (): JSX.Element => {
+    const navigate = useNavigate();
+
+    const [loading, setLoading] = useState<boolean>(false)
+    const [check, setCheck] = useState<Check>({
+        transporter: false,
+        shipper: false
+    })
+    const { transporter, shipper } = check
     const onClick = async (e: any) => {
         e.preventDefault()
-        console.log("Submitted");
+        console.log("Login");
+        if (transporter) {
+            navigate('/transporter/trucks')
+        } else {
+            navigate('/shipper/shipments')
+        }
+    }
 
+    const toggleCheck = (type: string) => {
+        if (type == 'transporter') {
+            setCheck(check => ({
+                shipper: false,
+                transporter: !transporter
+            }))
+        }
+        else {
+            setCheck(check => ({
+                transporter: false,
+                shipper: !shipper
+            }))
+        }
     }
     return (
         <>
@@ -32,6 +62,28 @@ const Home = (): JSX.Element => {
                     <h2 className='py-2 text-dark'> Kargo Tech </h2>
                     <hr />
                     <Container className='mt-5'>
+                        <Row className='mt-4 w-auto justify-content-center'>
+                            <Col sm md={4} lg={5} className="px-5">
+
+                                <div className="form-check text-dark">
+                                    <input className="form-check-input" type="checkbox" checked={transporter} defaultChecked={false} onChange={() => { toggleCheck('transporter') }} id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Transporter
+                                    </label>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row className='mt-4 w-auto justify-content-center'>
+                            <Col sm md={4} lg={5} className="px-5">
+
+                                <div className="form-check text-dark">
+                                    <input className="form-check-input" type="checkbox" checked={shipper} defaultChecked={false} onChange={() => { toggleCheck('shipper') }} id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Shipper
+                                    </label>
+                                </div>
+                            </Col>
+                        </Row>
                         <Row className='mt-4 justify-content-center'>
                             {
                                 loading ?
@@ -39,12 +91,13 @@ const Home = (): JSX.Element => {
                                         <span className="visually-hidden">Loading...</span>
                                     </Spinner>
                                     :
-                                    <Button onClick={onClick} className='w-auto' variant="primary" type="submit" style={{ fontSize: '1.3rem' }}>
+                                    <Button onClick={onClick} className='w-auto' variant="primary" type="submit" style={{ fontSize: '1.5rem' }}>
                                         Login
                                     </Button>
                             }
 
                         </Row>
+
                     </Container>
                 </div>
 
