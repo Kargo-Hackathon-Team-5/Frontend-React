5afd8fdb13203b7dda883a476b891640f74e38ca
davidf1000 <davidfauzi29@gmail.com>
2022-08-13 11:13:34 +0700

    add href to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,16 +10,16 @@ const TopNavBar = ({ type } : TopNavProps): JSX.Element => {
     return (
         <Navbar bg="light" variant="light">
             <Container className="justify-content-start">
-                <Navbar.Brand href="#home">Kargo Excellerate</Navbar.Brand>
+                <Navbar.Brand href="/">Kargo Excellerate</Navbar.Brand>
                 <Nav className="me-10">
                     {type == 'transporter' &&
                         <>
-                            <Nav.Link href="#features">Trucks</Nav.Link>
-                            <Nav.Link href="#pricing">Drivers</Nav.Link>
+                            <Nav.Link href="/transporter/trucks">Trucks</Nav.Link>
+                            <Nav.Link href="/transporter/drivers">Drivers</Nav.Link>
                         </>
                     }
                     {type == 'shipper' &&
-                        <Nav.Link href="#home">Shipments</Nav.Link>
+                        <Nav.Link href="/shipper/shipments">Shipments</Nav.Link>
 
                     }
 
